8e70634154ec2707ee7daea93b290a884d5e4ce5
jamespericles <63256479+jamespericles@users.noreply.github.com>
2020-07-13 17:56:50 -0400

    finished readme template

--- index.js
+++ index.js
@@ -6,61 +6,64 @@ const writeFileAsync = util.promisify(fs.writeFile);
 
 // array of questions for user
 function promptUser() {
-    return inquirer.prompt([
-      {
-        type: "input",
-        name: "projectTitle",
-        message: "What is the title of your project?"
-      },
-      {
-        type: "input",
-        name: "description",
-        message: "What is the description of your project?"
-      },
-      {
-        type: "input",
-        name: "installation",
-        message: "What instructions are necessary to install your project?"
-      },
-      {
-        type: "input",
-        name: "usage",
-        message: "How would you describe the usage of your project"
-      },
-      {
-        type: "list",
-        name: "license",
-        message: "Which License would you like to use?"
-        choices: []
-      },
-      {
-        type: "input",
-        name: "contributing",
-        message: "If you would like people to contribute to your work, please explain how they can do that."
-      },
-      {
-        type: "input",
-        name:"tests",
-        message: "Please describe any tests you conducted and their results."  
-      },
-      {
-        type: "list",
-        name:"questions",
-        message:"Please type what you would like for the <Questions> section of the document."
-      },
-      {
-        type: "input",
-        name: "github",
-        message: "Please enter your github username and repository URL for this project."  
-      }
-    ]);
-  }
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "projectTitle",
+      message: "What is the title of your project?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "What is the description of your project?",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "What instructions are necessary to install your project?",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "How would you describe the usage of your project",
+    },
+    //   {
+    //     type: "list",
+    //     name: "license",
+    //     message: "Which License would you like to use?"
+    //     choices: []
+    //   },
+    {
+      type: "input",
+      name: "contributing",
+      message:
+        "If you would like people to contribute to your work, please explain how they can do that.",
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: "Please describe any tests you conducted and their results.",
+    },
+    {
+      type: "list",
+      name: "questions",
+      message:
+        "Please type what you would like for the <Questions> section of the document.",
+    },
+    {
+      type: "input",
+      name: "github",
+      message:
+        "Please enter your github username and repository URL for this project.",
+    },
+  ]);
+}
 
 // function to write README file
 // function writeToFile(fileName, data) {}
 
 function generateREADME(answers) {
-    return `
+  return `
     #${answers.projectTitle}
     ---
     ##Table of Contents
@@ -75,17 +78,22 @@ function generateREADME(answers) {
     ###[Github](#Github)
     ---
     ###<a name="Description"></a>Description
+    ${answers.description}
     ###<a name="Installation"></a>Installation
+    ${answers.installation}
     ###<a name="Usage"></a>Usage
+    ${answers.usage}
     ###<a name="License"></a>License
+
     ###<a name="Contributing"></a>Contributing
+    ${answers.contributing}
     ###<a name="Tests"></a>Tests
+    ${answers.tests}
     ###<a name="Questions"></a>Questions
+    ${answers.questions}
     ###<a name="Github"></a>Github
-
-
-    `
-
+    ${answers.github}
+    `;
 }
 
 // function to initialize program
@@ -93,7 +101,7 @@ async function init() {
   try {
     const answers = await promptUser();
 
-    const html = generateREADME(answers);
+    const README = generateREADME(answers);
 
     await writeFileAsync("README.md", md);
 
